85da621228d36c78d9578e7aa29824e8d5a11dd7
GustavoBoscoGit <gustavobosco1@gmail.com>
2021-08-19 18:06:55 -0300

    Update novomain.dart

--- lib/novomain.dart
+++ lib/novomain.dart
@@ -1,33 +1,101 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  const BytebankApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        body: ListaTranferencias(),
-        appBar: AppBar(
-          title: Text('Tranferencias'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Icon(Icons.add),
-          backgroundColor: Colors.blue,
-        ),
+        body: FormularioTransferencia(),
       ),
-    ));
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  final TextEditingController _controladorCampoNumeroConta =
+      TextEditingController();
+  final TextEditingController _controladorCampoValor = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criando Transferêcia'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _controladorCampoNumeroConta,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                labelText: 'Numero da conta',
+                hintText: '0000',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: TextField(
+              controller: _controladorCampoValor,
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+              decoration: InputDecoration(
+                icon: Icon(Icons.monetization_on),
+                labelText: 'Valor',
+                hintText: '0.00',
+              ),
+              keyboardType: TextInputType.number,
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              final int numeroConta =
+                  int.parse(_controladorCampoNumeroConta.text);
+              final double valor = double.parse(_controladorCampoValor.text);
+              if (numeroConta != null && valor != null) {
+                final transferenciaCriada = Tranferencia(valor, numeroConta);
+                debugPrint('$transferenciaCriada');
+              }
+            },
+            child: Text('Confirmar'),
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 class ListaTranferencias extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: [
-        ItemTransferencia(Tranferencia(100.0, 1000)),
-        ItemTransferencia(Tranferencia(200.0, 1001)),
-        ItemTransferencia(Tranferencia(300.0, 1002)),
-      ],
-    );
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Tranferencias'),
+        ),
+        body: Column(
+          children: [
+            ItemTransferencia(Tranferencia(100.0, 1000)),
+            ItemTransferencia(Tranferencia(200.0, 1001)),
+            ItemTransferencia(Tranferencia(300.0, 1002)),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue,
+        ));
   }
 }
 
@@ -52,6 +120,11 @@ class ItemTransferencia extends StatelessWidget {
 class Tranferencia {
   final double valor;
   final int numeroConta;
+
+  @override
+  String toString() {
+    return 'Tranferencia{valor: $valor, numeroConta: $numeroConta}';
+  }
 
   Tranferencia(this.valor, this.numeroConta);
 }
