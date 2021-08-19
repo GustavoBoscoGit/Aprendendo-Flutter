fafa7499f647fe0ff343fb8d904f52a261370226
GustavoBoscoGit <gustavobosco1@gmail.com>
2021-08-19 11:24:33 -0300

    first commit

---
+++ lib/novomain.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      Column(
+        children: <Widget>[
+          Text(
+            'Deliver features faster',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Crat beatiful Uis',
+            textDirection: TextDirection.ltr,
+          ),
+          Text(
+            'Crat beatiful Uis',
+            textDirection: TextDirection.ltr,
+          ),
+          Expanded(
+            child: FittedBox(
+              fit: BoxFit.contain,
+              child: const FlutterLogo(),
+            ),
+          ),
+        ],
+      ),
+    );
