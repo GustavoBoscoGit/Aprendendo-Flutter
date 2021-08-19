47274313586fe720a36a7f47efa16d998734e9a1
GustavoBoscoGit <gustavobosco1@gmail.com>
2021-08-19 17:18:59 -0300

    Update novomain.dart

--- lib/novomain.dart
+++ lib/novomain.dart
@@ -1,30 +1,57 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      Column(
-        children: <Widget>[
-          Text(
-            'Deliver features faster',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Crat beatiful Uis',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'Crat beatiful Uis',
-            textDirection: TextDirection.ltr,
-          ),
-          Text(
-            'testando github commit',
-            textDirection: TextDirection.ltr,
-          ),
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.contain,
-              child: const FlutterLogo(),
-            ),
-          ),
-        ],
+void main() => runApp(MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        body: ListaTranferencias(),
+        appBar: AppBar(
+          title: Text('Tranferencias'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+          backgroundColor: Colors.blue,
+        ),
+      ),
+    ));
+
+class ListaTranferencias extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: [
+        ItemTransferencia(Tranferencia(100.0, 1000)),
+        ItemTransferencia(Tranferencia(200.0, 1001)),
+        ItemTransferencia(Tranferencia(300.0, 1002)),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Tranferencia _tranferencia;
+
+  ItemTransferencia(this._tranferencia);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_tranferencia.valor.toString()),
+        subtitle: Text(_tranferencia.numeroConta.toString()),
       ),
     );
+  }
+}
+
+class Tranferencia {
+  final double valor;
+  final int numeroConta;
+
+  Tranferencia(this.valor, this.numeroConta);
+}
