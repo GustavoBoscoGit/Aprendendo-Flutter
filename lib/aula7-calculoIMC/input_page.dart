ce6358bccab412b54e94ef11ccc0a2ac7c62e97e
GustavoBoscoGit <gustavobosco1@gmail.com>
2021-09-23 20:23:05 -0300

    Update input_page.dart

--- lib/aula7-calculoIMC/input_page.dart
+++ lib/aula7-calculoIMC/input_page.dart
@@ -9,11 +9,65 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Calculadora de IMC'),
-      ),
-      body: Center(
-        child: Text('Texto'),
+        appBar: AppBar(
+          title: Text('Calculadora de IMC'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ReusableCard(
+                color: Color(0xFF1D1F33),
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                  Expanded(
+                    child: ReusableCard(
+                      color: Color(0xFF1D1F33),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color color;
+
+  ReusableCard({required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
